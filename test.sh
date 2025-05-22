34eb9a300c18003b270b6f451d2ae4da1e30115c
spevnev <spevnev16@gmail.com>
2025-05-21 20:34:46 -0400

    Improve test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-BIN="./build/clox"
-
 RESET="\e[0m"
 RED="\e[1;31m"
 GREEN="\e[1;32m"
 BLUE="\e[1;34m"
+
+bin="./build/clox"
 
 make TEST=1 -B
 ret=$?
@@ -16,28 +16,28 @@ passed=0
 failed=0
 for file in $(find tests -type f); do
     expected=$(grep '// ' $file | grep -v '///' | sed -re 's/.*\/\/ (.+)/\1/')
-    output=$($BIN $file 2>&1)
+    output=$($bin $file 2>&1)
     name=$(echo $file | cut -d/ -f2-)
 
     if [ "$output" = "$expected" ]; then
         echo -e $GREEN"[PASSED] $name"$RESET
-        ((passed++))
+        ((passed += 1))
     else
         echo -e $RED"[FAILED] $name"$RESET
-        ((failed++))
+        ((failed += 1))
 
-        echo -e $BLUE"\tExpected:"$RESET
-        echo "$expected" | sed -e 's/^/\t/'
-        echo -e $BLUE"\tFound:"$RESET
-        echo "$output" | sed -e 's/^/\t/'
+        echo -e $BLUE"    Expected:"$RESET
+        echo "$expected" | sed -e 's/^/    /'
+        echo -e $BLUE"    Found:"$RESET
+        echo "$output" | sed -e 's/^/    /'
     fi
 done
 
 echo ""
 echo "Total:"
-echo -e "\tPassed: $passed"
-echo -e "\tFailed: $failed"
+echo "    Passed: $passed"
+echo "    Failed: $failed"
 
-if [[ $failed > 0 ]]; then
+if [ $failed -gt 0 ]; then
     exit 1
 fi
